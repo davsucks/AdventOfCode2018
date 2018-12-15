2a4f7415783ff759098d212acac6d992a531862d
dsucksto <dsucksto@thoughtworks.com>
2018-12-15 17:20:37 -0500

    Part two

--- day_5/src/main/kotlin/Main.kt
+++ day_5/src/main/kotlin/Main.kt
@@ -2,5 +2,8 @@ import java.io.File
 
 fun main() {
     val file = File("day_5/src/resources/input.txt")
+    println("Part one!")
     PartOne(file)
+    println("Part two!")
+    PartTwo(file)
 }
---
+++ day_5/src/main/kotlin/PartTwo.kt
@@ -0,0 +1,16 @@
+import java.io.File
+import PolymerReducer.capitalize
+
+
+class PartTwo(input: File) {
+    init {
+        val polymer = input.readLines().first().trim()
+        val reductionList = listOf('a'..'z').flatten().map { char ->
+            val capitalization = char.capitalize()
+            val reduction = PolymerReducer.reduce(polymer.filterNot { it == char || it == capitalization })
+            println("Filtering $capitalization/$char results in a length of ${reduction.length}")
+            reduction
+        }
+        println("Shortest reduction was ${reductionList.sortedBy { it.length }.first().length}")
+    }
+}
