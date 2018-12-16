6d7f0d62a8b8ec147d8cef1ad9089c1ce6a52e06
dsucksto <dsucksto@thoughtworks.com>
2018-12-16 09:34:23 -0500

    Day 7 boilerplate

---
+++ day_7/src/test/kotlin/DummyTest.kt
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DummyTest {
+    @Test
+    fun `it does nothing`() {
+        assertThat(true).isTrue()
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "advent_of_code"
-include("day_3", "day_4", "day_5", "day_6")
+include("day_3", "day_4", "day_5", "day_6", "day_7")
